b9094ab14cc1b40e3dacad91cd5e26fc0b626b5e
Andy Shu Xin <andyshuxin@gmail.com>
2015-10-06 22:14:36 +0800

    Add result page

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,30 @@ require('styles/App.css');
 import React from 'react/addons';
 import Cover from './Cover';
 import QACard from './QACard';
+import ResultPage from './ResultPage';
 
 let AppComponent = React.createClass({
 
   getInitialState: () => ({
     stage: 'cover',
+    totalScore: 0,
     questionSerial: 0
   }),
 
   handleOptionClick: function() {
+
+    let currentSerial = this.state.questionSerial;
+    let totalQuestionCount = this.props.survey.length;
+    let nextStage = 'qa';
+
+    if (currentSerial + 1 === totalQuestionCount) {
+      nextStage = 'result';
+    }
+
+
     this.setState({
-      questionSerial: this.state.questionSerial + 1
+      questionSerial: this.state.questionSerial + 1,
+      stage: nextStage,
     });
   },
 
@@ -27,15 +40,16 @@ let AppComponent = React.createClass({
 
   render: function() {
 
-
     if (this.state.stage === 'cover') {
       return <Cover coverClickHandler={this.startQuestion} />
+
     } else if (this.state.stage === 'qa') {
       let serial = this.state.questionSerial;
       let qa = this.props['survey'][serial];
       return <QACard qa={qa} optionClickHandler={this.handleOptionClick} />
+
     } else {
-      return <Result />
+      return <ResultPage score={this.state.totalScore} />
     }
 
   }
---
+++ src/components/ResultPage.js
@@ -0,0 +1,11 @@
+require('normalize.css');
+require('styles/App.css');
+
+import React from 'react/addons';
+
+let ResultPage = ({score}) =>
+  <div>
+    Result: {score}
+  </div>
+
+export default ResultPage;
