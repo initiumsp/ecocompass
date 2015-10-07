576f22bfd25121a009f997667b75976e6a070dfc
Andy Shu Xin <andyshuxin@gmail.com>
2015-10-07 12:55:39 +0800

    Move data.js to sources/ folder

--- src/components/run.js
+++ src/components/run.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Main'
-import survey from './data'
+import survey from './../sources/data'
 
 // Render the main component into the dom
 ReactDOM.render(<App survey={survey} />, document.getElementById('app'))
