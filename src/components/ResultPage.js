70f15b5f081a81508dab0fbdd299b60566362261
Andy Shu Xin <andyshuxin@gmail.com>
2015-10-07 17:17:10 +0800

    Add result comments and final notice to the ResultPage

--- src/components/ResultPage.js
+++ src/components/ResultPage.js
@@ -2,10 +2,25 @@ require('normalize.css')
 require('styles/App.css')
 
 import React from 'react'
+import result_commments from '../sources/result_comments'
+import final_notice from '../sources/final_notice'
 
-let ResultPage = ({score}) =>
-  <div>
-    Result: {score}
-  </div>
+let ResultPage = ({score}) => {
+  let commentText
+
+  for (let comment of result_commments) {
+    if ((score >= comment.minScore) && (score <= comment.maxScore)) {
+      commentText = comment.commentText
+    }
+  }
+
+  return (
+    <div>
+      <div>Result: {score}</div>
+      <div>{commentText}</div>
+      <div>{final_notice}</div>
+    </div>
+  )
+}
 
 export default ResultPage
