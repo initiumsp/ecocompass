a3d4a5879572ccc258d0d9130ac9014fd2b2bd00
yyfjmb <yyfjmb@hotmail.com>
2015-10-07 16:27:19 +0800

    stylize it and add more questions

--- src/components/QACard.js
+++ src/components/QACard.js
@@ -5,10 +5,37 @@ import React from 'react/addons'
 import QuestionText from './QuestionText'
 import OptionList from './OptionList'
 
+let leftImage = require('../images/left.png')
+let rightImage = require('../images/right.png')
+let upImage = require('../images/up.png')
+let downImage = require('../images/down.png')
+
+//let leftUpImage = require('../images/left-up.png')
+//let rightUpImage = require('../images/right-up.png')
+let leftDownImage = require('../images/left-down.png')
+let rightDownImage = require('../images/right-down.png')
+
+
 let QACard = ({qa, optionClickHandler}) =>
-  <div>
-    <QuestionText text={qa.question} />
-    <OptionList options={qa.options} optionClickHandler={optionClickHandler} />
+  <div id="wrapper">
+    <div class="row">
+      <div id="leftUp"></div>
+      <div id="up"></div>
+      <div id="rightUp"></div>
+    </div>
+    <div class="row">
+      <div id="left"></div>
+      <div id="middle">
+        <QuestionText text={qa.question} />
+        <OptionList options={qa.options} optionClickHandler={optionClickHandler} />
+      </div>
+      <div id="right"></div>
+    </div>
+    <div class="row">
+      <div id="leftDown"></div>
+      <div id="down"></div>
+      <div id="rightDown"></div>
+    </div>
   </div>
 
 export default QACard
