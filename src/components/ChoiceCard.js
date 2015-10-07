16905b7018e1c667e092f61d3cc4dca128325d73
yyfjmb <yyfjmb@hotmail.com>
2015-10-07 19:13:09 +0800

    responsive on mobile device

--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -7,24 +7,18 @@ import OptionList from './OptionList'
 
 let ChoiceCard = ({qa, optionClickHandler}) =>
   <div id='wrapper'>
-    <div className='row' id='first'>
       <div id='leftUp'></div>
       <div id='up'></div>
       <div id='rightUp'></div>
-    </div>
-    <div className='row' id='second'>
       <div id='left'></div>
       <div id='middle'>
         <QuestionText text={qa.question} />
         <OptionList options={qa.options} optionClickHandler={optionClickHandler} />
       </div>
       <div id='right'></div>
-    </div>
-    <div className='row' id='third'>
       <div id='leftDown'></div>
       <div id='down'></div>
       <div id='rightDown'></div>
-    </div>
   </div>
 
 export default ChoiceCard
